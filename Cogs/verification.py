eedfcf2e85d432159040faf93d3690e4ff4d67a1
compil3 <luca.tenuta@gmail.com>
2021-10-14 08:05:07 -0400

    fixed various issues with command.

--- Cogs/verification.py
+++ Cogs/verification.py
@@ -1,19 +1,30 @@
-import logging, datetime, os, requests
-from discord.app.commands import SlashCommand, slash_command
 import discord
-from discord.ext.commands.help import Paginator
-from pymongo import MongoClient, database
+from discord import interactions
 from discord.ext import commands
+from discord.app.commands import SlashCommand, slash_command
 
-PASS = os.environ.get("PASS")
-cluster = MongoClient(
-    f"mongodb+srv://nation_user:{PASS}@cluster0.bhwnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-)
+import logging, datetime, os, requests
+from dotenv.main import load_dotenv
+from os import environ
+from pymongo.message import delete
+
+from pymongo import MongoClient
+from .Extensions.paging import PaginatorView
+import motor.motor_asyncio as motor
+
+
+# PASS = os.environ.get("PASS")
+load_dotenv(".env")
+PASS = environ.get("MONGO_BOT_PASS")
+print(PASS)
+# cluster = MongoClient(
+#     f"mongodb+srv://pcn_bot:{PASS}@cluster0.bhwnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+# )
+cluster = motor.AsyncIOMotorClient(f"{environ.get('CONNECTION')}")
 db = cluster["Nation"]
 collection = db["verification"]
-paginator = Paginator()
 format = "%b %d %Y %I:%M%p"
-
+# paginator = Paginator
 guild_id = [
     689119429375819951,
 ]
@@ -23,12 +34,15 @@ class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # TODO: Fix up all the queries
+
     @slash_command(
         name="add",
         description="Add your Gamer-tag to the verification queue",
         guild_ids=guild_id,
     )
     async def add(self, ctx, gamertag: str):
+        await ctx.defer(ephemeral=True)
         try:
             test_id = collection.find({"discord_id": ctx.author.id}).count() > 0
             embed = discord.Embed(title="Verification System")
@@ -49,11 +63,11 @@ class Verification(commands.Cog):
                 now = datetime.datetime.now()
                 tstamp = now.strftime(format)
                 post = {
-                    "discord_id": ctx.author.id,
+                    "_id": ctx.author.id,
                     "discord_name": ctx.author.name + "#" + ctx.author.discriminator,
                     "gamertag": gamertag,
                     "status": "In Queue",
-                    "reason": "New",
+                    "reason": "New Application",
                     "updated": tstamp,
                 }
                 collection.insert_one(post)
@@ -65,46 +79,125 @@ class Verification(commands.Cog):
             logging.error(e)
         await ctx.respond(embed=embed)
 
-    @slash_command(name="check", description="Check your verification status.", guild_ids=guild_id)
+    @slash_command(
+        name="check", description="Check your verification status.", guild_ids=guild_id
+    )
     @commands.has_role("New")
     async def check(self, ctx):
-        result = collection.find({"discord_name": ctx.author.name + "#" + ctx.author.discriminator})
-        for user in result:
-            if user['status'] == 'Denied':
-                try:
-                    gt = user['gamertag']
-                    embed = discord.Embed(color=ctx.author.color)
-                    embed - discord.Embed(
-                        title="PCN Discord Verification",
-                        description=f"```Your GT: {gt} ```"
-                    )
-                    embed.add_field(name="Application Status", value=user['status'])
-                    embed.add_field(name='Reason', value=user['reason'], inline=True)
-                    embed.add_field(name='Updated', value=user['updated'], inline=False)
-                    await ctx.author.send(embed=embed)
-                    await ctx.send(f'{ctx.author.mention} Please check your DMs.', delete_after=5)
-                    break
-                except Exception as e:
-                    await ctx.send(f"{ctx.author.mention}, DMs must be enabled to retreive the status of your verification.")
-                    logging.info(f"User {ctx.author} does not have DMs enbled: {e}")
-                    break
-    
-    @slash_command(name='queue', description="View verification queue.", guild_ids=guild_id)
+        await ctx.defer(ephemeral=True)
+        _status = None
+        try:
+            applicant = await collection.find_one({"_id": ctx.author.id})
+
+            if applicant["_id"] == ctx.author.id:
+                if applicant["status"] == "Denied":
+                    _status = f"**Status:*** :no_entry:  **{applicant['status']}**"
+                    _reason = applicant["reason"]
+                    _updated = applicant["updated"]
+                else:
+                    _status = f"**Status**: :warning: **{applicant['status']}**"
+                    _reason = f"**{applicant['reason']}**"
+                    _updated = applicant["updated"]
+            else:
+                print("Error")
+            embed = discord.Embed(color=ctx.author.color)
+            embed = discord.Embed(
+                title="PCN Discord Verification Status", description=_status
+            )
+            embed.add_field(name="**Reason**", value=_reason, inline=False)
+            embed.add_field(name="Last Updated", value=_updated, inline=False)
+
+        except Exception as e:
+            print(e)
+        await ctx.respond(embed=embed, ephemeral=True)
+
+    # TODO: Add buttons to Approve/Deny etc and then ask for a reason if Denied/Pending Reviewed/Waiting etc
+    @slash_command(
+        name="queue", description="View verification queue.", guild_ids=guild_id
+    )
     @commands.has_permissions(kick_members=True)
     async def queue(self, ctx):
+        print("Entered queue command")
+        await ctx.defer(ephemeral=True)
+
         embeds = []
         result = collection.find({"status": "In Queue"})
         embed = discord.Embed(color=ctx.author.color)
         for user in result:
             embed = discord.Embed(
                 title="PCN Discord Queue System",
-                description=f"Gamer tag: ```{user['gamertag']}```"
+                description=f"Gamer tag: ```{user['gamertag']}```",
             )
-            embed.add_field(name="Discord User Name", value=user["discord_name"], inline=False)
-            embed.add_field(name='Status', value=user['status'], inline=True)
-            embed.add_field(name='Reason', value=user['reason'], inline=True)
-            embed.add_field(name='Updated', value=user['updated'], inline=False)
+            embed.add_field(
+                name="Discord User Name", value=user["discord_name"], inline=False
+            )
+            embed.add_field(name="Status", value=user["status"], inline=True)
+            embed.add_field(name="Reason", value=user["reason"], inline=True)
+            embed.add_field(name="Updated", value=user["updated"], inline=False)
+            embed.set_footer(text=f"Discord ID: {user['discord_id']}")
+            embeds.append(embed)
+        try:
+            view = PaginatorView(embeds, ctx)
+            # await ctx.respond(embed=embeds[0], view=view)
+            # view.message = await ctx.original_message()
+            view.message = await ctx.respond(content="_ _", embed=embeds[0], view=view)
+        except Exception as e:
+            print(e)
 
+    @commands.has_permissions(kick_members=True)
+    @slash_command(
+        name="approve", description="Approve member application", guild_id=guild_id
+    )
+    async def approve(self, ctx, member: discord.Member = None):
+        try:
+            await ctx.defer(ephemeral=True)
+            member = discord.Guild.get_member(id)
+            _user = member.name + "#" + member.discriminator
+            now = datetime.datetime.now()
+            tstamp = now.strftime(format)
+
+            if collection.find({"discord_name": _user}).count() < 1:
+                await ctx.send(
+                    f"{member.name} not found.  Please try again or contact Spillshot",
+                    delete_after=5,
+                )
+            else:
+                collection.update(
+                    {"discord_name": _user},
+                    {
+                        "$set": {
+                            "status": "Completed",
+                            "reason": "Approved",
+                            "updated": tstamp,
+                        }
+                    },
+                )
+                await member.remove_roles(843896103686766632)
+                await member.add_roles(843899510483976233)
+                await ctx.respond(f"{member.name} approved.", delete_after=5)
+                await ctx.member.send("You have been approved in Discord.")
+        except Exception as e:
+            print(e)
+
+    @commands.has_permissions(kick_members=True)
+    @slash_command(
+        name="deny", description="Deny member application", guild_ids=guild_id
+    )
+    async def deny(self, ctx, member: discord.Member, reason: str):
+
+        await ctx.defer(ephemeral=True)
+        _user = member.name + "#" + member.discriminator
+        now = datetime.datetime.now()
+        tstamp = now.strftime(format)
+
+        if collection.find({"discord_name": _user}).count() < 1:
+            await ctx.respond(f"{member.name} not found.  Please try again.")
+        else:
+            collection.update(
+                {"discord_name": _user},
+                {"$set": {"status": "Denied", "reason": reason, "updated": tstamp}},
+            )
+            await ctx.respond(f"{member.name} updated.")
 
 
 def setup(bot):
